a8f62c88663307af8969e7d4aa27b1cf63c7c21a
Alexis2879 <https://alexis2879.github.io/trabajo-salv/>
2022-03-01 20:52:08 -0500

    trabajos

---
+++ semana3/dia5/probatida.js
@@ -0,0 +1,24 @@
+alert("Hola Mundo")
+
+const numero1 = 10;
+const numero2 = 20;
+
+/*let resultado = 0;
+
+resultado = numero1 + numero2;
+
+console.log(resultado);*/
+
+/*const numero3 = 50;
+const numero4 = 350;
+let resultado = 0;
+resultado = numero3 + numero4;
+console.log(resultado)*/
+/*const numero4 = 200;
+const numero5 = 300;
+let resultado = 0;
+resultado = numero4 + numero5;
+console.log(resultado);*/
+
+console.log("resultado: " + numero1 + " + " + numero2 + " = " + resultado);
+console.log("sumatoria:", resultado);
---
+++ semana4/dia1/ejerciciogio.js
@@ -0,0 +1,13 @@
+/*alert("Hola Mundo");*/
+
+/*let saludo = "Hello World"
+console.log(saludo);*/
+
+/*const num1 = 3;
+const num2 = 5;
+
+let resultado = 0;
+resultado = num1 + num2;
+console.log(resultado);*/
+
+/*Bucles*/
