722c2763387c1bbd1e0811c8ce2484d69d979379
Alexis2879 <https://alexis2879.github.io/trabajo-salv/>
2022-03-09 23:13:15 -0500

    sem7reto12

--- semana7/dia1/reto12/index.js
+++ semana7/dia1/reto12/index.js
@@ -1,16 +1,3 @@
-// como consumo el API?
-const URL = "https://6226bedf2dfa5240180c3e36.mockapi.io/fiction_movies";
-
-const getMovies = async () => {
-  try {
-    const response = await fetch(URL);
-    const data = await response.json();
-    return data;
-  } catch (error) {
-    console.log(error.message);
-  }
-};
-
 // haremos una funcion que se encargue de agrupar las pelicualas
 const groupMovies = async () => {
   // podemos crear 3 arreglos
